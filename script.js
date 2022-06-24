410c14245a5a101ad6075b6e0baa9b7af188cf91
Offiongwhilley <offiongwhilley@gmail.com>
2022-06-24 14:55:38 +0100

    updating my code

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-alert('Name: Offiong Ekpenyong; Height: 5.5ft; Country: Nigeria.'
-)
+// alert('Name: Offiong Ekpenyong; Height: 5.5ft; Country: Nigeria.'
+// )
+
+document.write('Name: Offiong Ekpenyong')
+document.write('<br>')
+
+document.write('Height: 5.5ft')
+document.write('<br>')
+
+document.write('Country: Nigeria')
+document.write('<br>')
